730179ee1011b0c9ee20324d72b4e4c63ea1dd9f
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-08-26 19:52:26 +0700

    Change Api URL

--- src/globals/config.js
+++ src/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'https://vuestore-server-murex.vercel.app/',
+  BASE_URL: 'https://ryan-shopcart-api.vercel.app/',
   BASE_IMAGE_URL: 'https://cdn.jsdelivr.net/gh/RyanBagusB/vuestore-server@main/public',
 };
 
